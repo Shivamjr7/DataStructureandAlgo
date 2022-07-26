79cb828917bc0f4cff431b8f283796b451d17466
Shivam Jari <shivamjr7@gmail.com>
2022-07-26 10:30:48 +0530

    single ele in sorted array

--- src/arrays/SingleElementInSortedArrays.java
+++ src/arrays/SingleElementInSortedArrays.java
@@ -4,27 +4,43 @@ public class SingleElementInSortedArrays {
 
 
     public static void main(String[] args) {
-        int [] arr  = {1,1,2,3,3,4,4,8,8};
+        int[] arr = {1};
+
         System.out.println(singleNonDuplicate(arr));
     }
 
-    public static  int singleNonDuplicate(int[] nums) {
+    public static int singleNonDuplicate(int[] nums) {
 
 
-        int start =0;
-        int end = nums.length -1;
+        int start = 0;
+        int end = nums.length - 1;
 
 
-        while(start <=end)
-        {
-            int mid = start + (end -start)/2;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
 
-
-            if(mid!=0 && mid!= nums.length-1 && nums[mid]!= nums[mid-1] && nums[mid]!= nums[mid+1])
+            // if both left and right ele are not equal , this is a single element
+            if (mid != 0 && mid != nums.length - 1 && nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])
                 return nums[mid];
 
+
+            // if left element is equal
+            if (mid != 0 && nums[mid] == nums[mid - 1]) {
+                if (mid % 2 != 0)  // example : 1,1,2,2,3
+                    start = mid + 1;
+                else
+                    end = mid - 1;   // 1,1,2,3,3,4,4,8,8
+            }// if right element is equal
+            else {
+                if (mid % 2 == 0)
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+
+
         }
-        return nums[start];
+        return nums[start - 1];
 
 
     }
